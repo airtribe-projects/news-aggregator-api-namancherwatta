36439fff6c4619cc7939d0517cb2ac6ac8ba9458
Naman <namanherwatta@gmail.com>
2025-10-17 22:13:57 +0530

    login added

--- controller/userController.js
+++ controller/userController.js
@@ -34,4 +34,28 @@ const userRegister = async (req, res) => {
   }
 };
 
-export { userRegister };
+const userLogin= async (req,res)=>{
+  try {
+    const { email, password } = req.body;
+    if(!email || !password){
+      res.status(400).json({message:"All fields are required"})
+    }
+    const userexists = await userModel.findOne({ email });
+    if (!userexists) {
+      return res.status(400).json({ message: `User with this email ${email} does not exist` });
+    }
+    const passwordcheck = await bcrypt.compare(password,userexists.password)
+    console.log(passwordcheck)
+    if(passwordcheck){
+      return res.status(200).json({message: `Hi ${userexists.name}, you have logged in.`})
+    }else{
+      return res.status(400).json({message: "Please type correct password."})
+    }
+
+  } catch (error) {
+    console.error("Error logging in user:",error)
+    return res.status(500).json({message: "Server error",error})
+  }
+}
+
+export { userRegister,userLogin };
--- routes/v1/userRoute.js
+++ routes/v1/userRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 const router= Router()
-import { userRegister} from "../../controller/userController.js";
+import { userRegister,userLogin} from "../../controller/userController.js";
 
 
 router.post("/register",userRegister)
+router.post("/login",userLogin)
 
 
 export default router
